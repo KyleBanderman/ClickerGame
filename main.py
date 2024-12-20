abfd47daabf3aa399b507fc55175ff7e80af3aa3
Kyle Banderman <kyle.banderman4675@gmail.com>
2024-12-20 00:30:38 -0600

    finished ui. need to implement upgrades

---
+++ main.py
@@ -0,0 +1,121 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import sys
+from upgrades import UpgradeModel
+
+    
+def close_window():
+    root.quit()
+    sys.exit()
+
+def increment_counter():
+    global clicker_counter
+    clicker_counter += 1
+    if clicker_counter < 1000000:
+        counter_label.configure(text = clicker_counter)
+    else:#TODO:replace this format with the cookie clicker format, i.e 1.456a, 2.679b
+        counter_label.configure(text = format(clicker_counter, 'e'))
+
+def open_settings():
+    test = "test"
+
+def on_mousewheel(event):
+    canvas.yview_scroll(-1*event.delta//120, "units")
+
+#create fullscreen root without top bar
+root = tk.Tk()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+root.geometry(f"{screen_width}x{screen_height}")
+root.attributes('-fullscreen', True)
+root.configure(bg="#D4D4D4")
+
+#create and position frames
+name_frame = tk.Frame(root, width=screen_width / 2 - 10, height=40, bg="red")
+name_frame.grid_rowconfigure(0, weight=1)
+name_frame.grid_columnconfigure(0, weight=1)
+name_frame.grid_propagate(False)
+name_frame.grid(row=0, column=0, pady=(10, 10), padx=(10, 3))
+
+counter_frame = tk.Frame(root, width=screen_width / 2 - 10, height=40, bg="orange")
+counter_frame.grid_rowconfigure(0, weight=1)
+counter_frame.grid_columnconfigure(0, weight=1)
+counter_frame.grid_propagate(False)
+counter_frame.grid(row=1, column=0, pady=(10,10), padx=(10,3))
+
+clicker_frame = tk.Frame(root, width=screen_width / 2 - 10, height = screen_height - 145, bg="yellow")
+clicker_frame.grid_rowconfigure(0, weight=1)
+clicker_frame.grid_columnconfigure(0, weight=1)
+clicker_frame.grid_propagate(False)
+clicker_frame.grid(row=2, column=0, pady=(10,10), padx=(10,3))
+
+settings_frame = tk.Frame(root, width=screen_width / 2 - 10, height=40, bg="green")
+settings_frame.grid_rowconfigure(0, weight=1)
+settings_frame.grid_columnconfigure(0, weight=1)
+settings_frame.grid_propagate(False)
+settings_frame.grid(row=0, column=1, pady=(10, 10), padx=(3, 10))
+
+upgrade_frame = tk.Frame(root, width=screen_width / 2 - 10, height = screen_height - 80, bg="blue")
+upgrade_frame.grid_rowconfigure(0, weight=1)
+upgrade_frame.grid_columnconfigure(0, weight=1)
+upgrade_frame.grid_propagate(False)
+upgrade_frame.grid(row=1, rowspan=2, column=1, pady=(10,10), padx=(3, 10))
+
+#load image for close window button
+close_image = Image.open("close_window_image.png")
+close_photo = ImageTk.PhotoImage(close_image)
+#create the close window button
+close_window_button = tk.Button(settings_frame, command=close_window, image=close_photo, height=30, width=30)
+close_window_button.configure(bg="white", fg="blue")
+close_window_button.grid(row=0, column=0, sticky="NE", padx=(5, 5), pady=(5,5))
+
+#load image for settings button
+settings_image = Image.open("settings_icon.jpg")
+settings_photo = ImageTk.PhotoImage(settings_image)
+#create the settings button
+settings_button = tk.Button(settings_frame, command=open_settings, image=settings_photo, height=30, width=30)
+settings_button.configure(bg="white", fg="blue")
+settings_button.grid(row=0, column=0, sticky="NE", padx=(5, 40), pady=(5,5))
+
+#create label for name of clicker run
+name_label = tk.Label(name_frame, text="Welcome to Clicker Game", font=("Arial", 30), bg="#D4D4D4")
+name_label.grid(row=0, sticky="NW")
+
+#create counter and label
+global clicker_counter
+clicker_counter = 0
+counter_label = tk.Label(counter_frame, text = clicker_counter, font=("Arial", 30), bg="#D4D4D4")
+counter_label.grid(row=0, column=0)
+
+counter_button = tk.Button(clicker_frame, command=increment_counter, text="Click", height=30, width=30, font=("Arial", 30))
+counter_button.grid(row=0, column=0)
+
+#create passive increment global for upgrades
+global passive_increment
+passive_increment = 0
+
+#create scrollable frame
+upgrade_canvas = tk.Canvas(upgrade_frame, height=100, bg="blue")
+canvas_frame = tk.Frame(upgrade_canvas)
+upgrade_scrollbar = tk.Scrollbar(upgrade_frame,orient="vertical", command=upgrade_canvas.yview)
+upgrade_canvas.create_window((0,0), window=canvas_frame, anchor="nw")
+canvas_frame.rowconfigure(0, weight=1)
+canvas_frame.columnconfigure(1, weight=1)
+
+#create upgrade buttons on startup
+
+upgrade_button_list = [1, 2, 3, 4, 5, 6, 6, 6 ,6 ,6]
+incremental_var = 0
+for item in upgrade_button_list:
+    button = tk.Button(canvas_frame, text=f"Upgrade {incremental_var+1}", command=increment_counter, height=5, width=105)
+    button.grid(row=incremental_var, column=0, sticky="news")
+    incremental_var += 1
+
+canvas_frame.update()
+upgrade_canvas.configure(yscrollcommand=upgrade_scrollbar.set, scrollregion="0 0 0 %s" % canvas_frame.winfo_height())
+
+
+upgrade_canvas.grid(row=0, column=0, sticky="news")
+upgrade_scrollbar.grid(row=0, column=10, sticky="ns")
+canvas_frame.bind("<MouseWheel>", on_mousewheel)
+root.mainloop()
---
+++ upgrades.py
@@ -0,0 +1,20 @@
+global id_counter
+id_counter = 1
+
+class UpgradeModel:
+    def __init__(self, id, counter_amount, amount, purchase_cost = 0):
+        self.id = id_counter
+        id_counter += 1
+        self.counter_amount = counter_amount
+        self.amount = amount
+        if self.amount == 0:
+            self.purchase_cost = 10 ** id
+        else:
+            self.purchase_cost = purchase_cost
+    
+    def update_passive_increment():
+        passive_increment += counter_amount
+
+    def purchase_new_upgrade():
+        amount += 1
+        purchase_cost = purchase_cost * 1.2
